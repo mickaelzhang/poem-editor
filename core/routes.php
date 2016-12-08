9ee7785b0d633b30c4507f5b5daa948c28fb3acf
Mickael Zhang <mickael.zhg@gmail.com>
2016-12-09 00:24:12 +0100

    [ADD] Editor page and route

--- core/routes.php
+++ core/routes.php
@@ -5,4 +5,8 @@ $app->get('/', function() use ($app) {
     return $app['twig']->render('pages/home.twig');
 })->bind('home');
 
+$app->get('/editeur', function() use ($app) {
+    return $app['twig']->render('pages/editor.twig');
+})->bind('editor');
+
 return $app;
