e68059ce931822738d0c43625ffb91347b5c65b4
alexandrechich <alexandrechich@users.noreply.github.com>
2016-12-11 23:12:52 +0100

    [UPDATE] route for themepage

--- core/routes.php
+++ core/routes.php
@@ -6,3 +6,9 @@ $app->get('/', function() use ($app) {
 })->bind('home');
 
 return $app;
+
+// THEMEPAGE
+$app->get('/theme', function() use ($app) {
+    return $app['twig']->render('pages/theme.twig');
+})->bind('theme');
+return $app;
