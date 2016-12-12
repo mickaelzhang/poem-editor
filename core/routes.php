1a89606d686772d75ad9bee6d126b37ba3a59a2f
CMarzin <corentinmarzin@gmail.com>
2016-12-12 10:51:53 +0100

    [FIX] fix routes and template page home

--- core/routes.php
+++ core/routes.php
@@ -5,10 +5,9 @@ $app->get('/', function() use ($app) {
     return $app['twig']->render('pages/home.twig');
 })->bind('home');
 
-return $app;
-
 // THEMEPAGE
 $app->get('/theme', function() use ($app) {
     return $app['twig']->render('pages/theme.twig');
 })->bind('theme');
+
 return $app;
