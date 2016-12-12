b8791ce13c830cda9db2db427673c9299a3a9c4c
alexandrechich <alexandrechich@users.noreply.github.com>
2016-12-12 18:04:16 +0100

    [UPDATE] routes

--- core/routes.php
+++ core/routes.php
@@ -1,13 +1,23 @@
 <?php
-
 // HOMEPAGE
 $app->get('/', function() use ($app) {
     return $app['twig']->render('pages/home.twig');
 })->bind('home');
 
-// THEMEPAGE
 $app->get('/theme', function() use ($app) {
     return $app['twig']->render('pages/theme.twig');
 })->bind('theme');
+
+$app->get('/rime', function() use ($app) {
+    return $app['twig']->render('pages/rime.twig');
+})->bind('rime');
+
+$app->get('/syllabe', function() use ($app) {
+    return $app['twig']->render('pages/syllabe.twig');
+})->bind('syllabe');
+
+$app->get('/vers', function() use ($app) {
+    return $app['twig']->render('pages/vers.twig');
+})->bind('vers');
 
 return $app;
