cb28687340f114d65056ad71eb9bfa698bd3af92
Mickael Zhang <mickael.zhg@gmail.com>
2016-12-13 01:22:21 +0100

    [ADD] SceneManager.js

---
+++ src/js/app/SceneManager.js
@@ -0,0 +1,4 @@
+export default class SceneManager {
+	constructor() {
+	}
+}
--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,9 @@
  * Main JS Controller
  * All Javascript Object should be initialised here
  */
+
+import SceneManager from './app/SceneManager'
+
 console.log('Javascript is linked!');
+
+new SceneManager()
